25448348ed5d56f48ea54acdc0cedb8580a77898
BLNRGH <blenoir@southfaceventures.com>
2015-09-27 22:55:32 +0100

    first implement

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # Constructs setters (provide caching of the inverse) 
+        #  and getters for a matrix and its inverse
+        # Returns the setters and getters
+        
+        inversedMatrixCached <- NULL
+        
+        # set matrix
+        setMatrix <- function(matrix) {
+                x <<- matrix
+                inversedMatrixCached <<- NULL
+        }
+        
+        # get matrix
+        getMatrix <- function() x
+        
+        # set inversed matrix
+        setInversedMatrix <- function(inversedMatrix) 
+                inversedMatrixCached <<- inversedMatrix
+        
+        # get inversed matrix
+        getInversedMatrix <- function() inversedMatrixCached
+        
+        # return list of functions
+        list(setMat = setMatrix, getMat = getMatrix,
+             setInvMat = setInversedMatrix,
+             getInvMat = getInversedMatrix)
+        
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        # Computes the inverse of a matrix 'x' 
+        #  (if not present in cache)
+        # Stores the computed inverse in cache
+        # Returns the computed inverse
+        
+        # check for cached inversed matrix
+        inversedMatrix <- x$getInvMat()
+        
+        # check if inversed matrix cache is present
+        if(!is.null(inversedMatrix)) {
+                message("getting cached data")
+                return(inversedMatrix)
+        }
+        
+        # get matrix for inversion
+        matrix <- x$getMat()
+        
+        # compute inversed matrix
+        inversedMatrix <- solve(matrix, ...)
+        
+        # set inversed matrix for chaching
+        x$setInvMat(inversedMatrix)
+        
+        # return inversed matrix
+        inversedMatrix
+        
 }
